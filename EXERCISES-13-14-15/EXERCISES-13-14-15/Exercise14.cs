b0c03ca5d99244a2b5ef02b8791fba0f6ba277d1
Maronda Anderson-Johnson <66698985+Maronda12@users.noreply.github.com>
2021-08-11 16:01:20 -0400

    Add files via upload

---
+++ EXERCISES-13-14-15/EXERCISES-13-14-15/Exercise14.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace EXERCISES_13_14_15
+{
+    public class Exercise14
+    {
+        public Exercise14()
+        {
+            int counter;
+            int square = 1;
+            const int maxValue = 10;
+
+
+            Console.WriteLine("Number  Square");
+            Console.WriteLine("-------------------");
+
+            for (counter = 1; counter <= maxValue; counter++)
+            {
+                square = counter * counter;
+                Console.WriteLine("{0}   {1}", counter, square);
+            }
+        }
+    }
+}
---
+++ EXERCISES-13-14-15/EXERCISES-13-14-15/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using System.Linq;
+
+namespace EXERCISES_13_14_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 13
+
+
+            bool goOn = true;
+            while (goOn == true)
+            {
+
+
+                //Get number 
+                Console.Write("Please enter a number: ");
+                int num = int.Parse((Console.ReadLine()));
+                int i;
+
+                /*For loop number entered by user, Decrementing by 1*/
+                for (i = num; i >= 1; i--)
+                {
+                    Console.WriteLine("\n" + i);
+                }
+
+
+
+                //Continue Loop
+                while (true)
+                {
+                    Console.WriteLine($"Would you like to continue ? y/n");
+                    string answer = Console.ReadLine();
+
+                    if (answer.ToLower() == "y")
+                    {
+                        goOn = true;
+                        break;
+                    }
+                    else if (answer.ToLower() == "n")
+                    {
+                        Console.WriteLine("Good Bye!");
+                        goOn = false;
+
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Im sorry I didnt understand that lets try again");
+                    }
+                }
+
+
+            }
+
+        }
+
+    }
+}
+
+ 
+    
+    
